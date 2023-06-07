fd12ab08dd63eb54e51e6695da6c309d28dbd202
muntasir_ac <muntasir@finder-lbs.com>
2023-06-07 10:38:31 +0600

    fixed database issues

--- dbconnection.py
+++ dbconnection.py
@@ -1,8 +1,16 @@
 from pymongo import MongoClient
+# from motor.motor_asyncio import AsyncIOMotorClient
+# from motor.motor_tornado import MotorClient
 
 
 def connect_db():
-    client = MongoClient('mongodb://localhost: 27017')
+    client = MongoClient('mongodb://localhost:27017')
     db = client.get_database('tor_test')
 
-    return db
+    return client
+
+# def connect_motor():
+#     # client = connect_db()
+#     motor_client = MotorClient('mongodb://localhost: 27017')
+
+#     return motor_client.tor_test
