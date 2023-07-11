c8e004bfd369b2b61f13b8461e775d5b9eb1f54d
MuntasirMac <muntasir120@gmail.com>
2023-07-11 10:07:56 +0600

    tested creating image thumbnail

--- image.py
+++ image.py
@@ -1,13 +1,31 @@
 import tornado.web
 import tornado.ioloop
+from PIL import Image
+from io import BytesIO
 
 class uploadImgHandler(tornado.web.RequestHandler):
-    def post(self):
-        files = self.request.files["fileImage"]
-        for f in files:
-            fh = open(f"upload/{f.filename}", "wb")
-            fh.write(f.body)
-            fh.close()
-        self.write(f"http://localhost:8881/img/{f.filename}")
     def get(self):
         self.render("index.html")
+
+    def post(self):
+        file = self.request.files["fileImage"][0]
+        # print(files['body'])
+        image = Image.open(BytesIO(file['body']))
+        th_image = image
+
+        size = (100, 100)
+        th_image.thumbnail(size)
+        thumb = th_image
+        # print(thumb.info)
+        th = open("/home/muntasir/thumbtornado.png",'wb')
+        bs = BytesIO()
+        thumb.save(bs, format='JPEG')
+        b_str = bs.getvalue()
+        bs.close()
+        th.write(b_str)
+        # for f in files:
+        #     fh = open(f"/{f.filename}", "wb")
+        #     fh.write(f.body)
+        #     fh.close()
+        # self.write(f"http://localhost:8881/img/{f.filename}")
+        # self.write('posted!')
